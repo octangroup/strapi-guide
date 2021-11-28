5c8cfeb6e37e3f8083879f0eb6ad176abb840763
musayann <musayann@octangrp.com>
2021-11-28 15:47:59 +0200

    chore: initialize project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '78e9c048445f62eba9713a0fdbaf8125'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13d96eed-91a2-4796-a461-b081b18322a6'
+};
